e4a144bcde34da216f0f77d564c8c565059950a8
Endie-James123 <ndopnnoabasi@gmail.com>
2025-10-07 14:38:23 +0100

    Added a linked bio file, just for practice and test sake

---
+++ Week 3/Exercises/app.ts
@@ -0,0 +1,14 @@
+
+
+export const bio: { name: string; role: string; email: string; frameworks: string[]; databases: string[]; languages: string[]; tools: string[]; interests: string[]; } = {
+  name: "Ndopnno-Abasi James",
+  role: "Backend Developer | Data Analyst | Emerging AI/ML Engineer",
+  email: "ndopnnoabasi@gmail.com",
+  languages: ["JavaScript", "TypeScript", "Python", "SQL"],
+  frameworks: ["Node.js", "NestJS", "Express.js", "React", "Bootstrap"],
+  databases: ["MongoDB", "PostgreSQL"],
+  tools: ["Git", "Docker", "Render", "Postman", "Railway"],
+  interests: ["Backend Development", "Data Analysis", "Artificial Intelligence", "Machine Learning"]
+};
+
+console.log(bio);
